0c58078e6c33809aeae8faeab3a40f56159f860f
Lin-zhibo <138888752+Lin-zhibo@users.noreply.github.com>
2026-01-16 17:00:01 +0800

    update icon

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,2 +1,3 @@
 // scripts/injector.js
 hexo.extend.injector.register('head_end', '<link rel="stylesheet" href="/css/custom.css">');
+hexo.extend.injector.register('body_end', '<script src="/js/tab-title.js"></script>');
